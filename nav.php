1409672f0e09a2a8080e0535ebe3b5e44dfcffae
Brandon Biernaczyk <brandon.biernaczyk@gmail.com>
2023-04-03 20:35:00 +0200

    added title for better navigation

--- nav.php
+++ nav.php
@@ -1,20 +1,17 @@
 <div class="controls">
-    <div class="control active-btn" data-id="home">
+    <div title="home" class="control active-btn" data-id="home">
         <a href="index.php"><i class="fas fa-home"></i></a>
     </div>
-    <div class="control" data-id="about">
+    <div title="about" class="control" data-id="about">
         <a href="#1"><i class="fas fa-user"></i></a>
     </div>
-    <div class="control" data-id="portfolio">
+    <div title="professional experience" class="control" data-id="portfolio">
         <a href="#4"><i class="fas fa-briefcase"></i></a>
     </div>
-    <div class="control" data-id="blogs">
+    <div title="skills" class="control" data-id="blogs">
         <a href="homelabs.php"><i class="far fa-newspaper"></i></a>
     </div>
-    <div class="control" data-id="contact">
+    <div title="contact me" class="control" data-id="contact">
         <a href="#"><i class="fas fa-envelope-open"></i></a>
     </div>
-</div>
-<div class="theme-btn">
-    <i class="fas fa-adjust"></i>
 </div>
