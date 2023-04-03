03a6d97734f4fd6056d0754ab0cf09f85843db21
Brandon Biernaczyk <brandon.biernaczyk@gmail.com>
2023-04-03 20:34:13 +0200

    created separated azure page

---
+++ azure.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include 'head.php' ?>
+
+<body class="main-content">
+  <main>
+    <section class="container active" id="portfolio">
+      <div class="main-title">
+        <h2>
+          Microsoft Azure <span>Home labs</span><span class="bg-text">My Work</span>
+        </h2>
+      </div>
+      <p class="port-text">
+        Here you can find out more about my MS AZURE experience. Down below i listed my home labs and specific things that i did and learned.
+      </p>
+      <div class="accordion">
+        <div class="accordion-header">
+          <h3>Section 1</h3>
+          <i class="fa fa-chevron-down"></i>
+        </div>
+        <div class="accordion-content">
+          <p>Content for section 1 goes here.</p>
+        </div>
+
+        <div class="accordion-header">
+          <h3>Section 2</h3>
+          <i class="fa fa-chevron-down"></i>
+        </div>
+        <div class="accordion-content">
+          <p>Content for section 2 goes here.</p>
+        </div>
+
+        <div class="accordion-header">
+          <h3>Section 3</h3>
+          <i class="fa fa-chevron-down"></i>
+        </div>
+        <div class="accordion-content">
+          <p>Content for section 3 goes here.</p>
+        </div>
+      </div>
+
+    </section>
+    <?php
+    include 'nav.php' ?>
+  </main>
+</body>
+
+</html>
