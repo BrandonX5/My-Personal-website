aa0b8fc4285a0154e1a768956ad61942dd159c2b
Brandon Biernaczyk <brandon.biernaczyk@gmail.com>
2023-06-17 01:48:36 +0200

    changed nav items (now can click on whole item)

--- nav.php
+++ nav.php
@@ -1,16 +1,23 @@
 <div class="controls">
-    <div title="home" class="control active-btn" data-id="home">
-        <a href="index.php"><i class="fas fa-home"></i></a>
-    </div>
-    <div title="about" class="control" data-id="about">
-        <a href="aboutme.php"><i class="fas fa-user"></i></a>
-    </div>
+    <a href="index.php">
+        <div title="home" class="control" data-id="home">
+            <i class="fas fa-home"></i>
+        </div>
+    </a>
+
+    <a href="aboutme.php">
+        <div title="about" class="control" data-id="about">
+            <i class="fas fa-user"></i>
+        </div>
+    </a>
     <div title="professional experience" class="control" data-id="portfolio">
         <a href="#4"><i class="fas fa-briefcase"></i></a>
     </div>
-    <div title="skills" class="control" data-id="blogs">
-        <a href="homelabs.php"><i class="far fa-newspaper"></i></a>
-    </div>
+    <a href="homelabs.php">
+        <div title="skills" class="control" data-id="blogs">
+            <i class="far fa-newspaper"></i>
+        </div>
+    </a>
     <div title="contact me" class="control" data-id="contact">
         <a href="#"><i class="fas fa-envelope-open"></i></a>
     </div>
