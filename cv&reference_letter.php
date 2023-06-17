c8ad65a00e3e0bf1012fd2ac0946909924fdf8bc
Brandon Biernaczyk <brandon.biernaczyk@gmail.com>
2023-06-17 13:14:04 +0200

    changed download section into php module

---
+++ cv&reference_letter.php
@@ -0,0 +1,12 @@
+<div class="btn-con" style="margin-bottom: 10px">
+    <a href="" class="main-btn">
+        <span class="btn-text">Download CV</span>
+        <span class="btn-icon"><i class="fas fa-download"></i></span>
+    </a>
+</div>
+<div class="btn-con">
+    <a href="/pdfs/Referencje od Infocentrum-1.pdf" target="_blank" class="main-btn">
+        <span class="btn-text">Download reference letter</span>
+        <span class="btn-icon"><i class="fas fa-download"></i></span>
+    </a>
+</div>
